87dc6f148f9f28499a143374df1d3304b515f100
Shamkhan Mutuskhanov <inkomman@gmail.com>
2023-06-19 13:43:44 +0300

    Initial Commit

---
+++ CodeLayout/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CodeLayout
+//
+//  Created by Shamkhan Mutuskhanov on 19.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
